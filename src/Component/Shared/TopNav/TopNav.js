caab84527c3fb14818f2d4fb2067edd827815ac2
Pervez <pervezhossain1998@gmail.com>
2023-06-11 14:57:18 +0600

    Nav ReArranged

--- src/Component/Shared/TopNav/TopNav.js
+++ src/Component/Shared/TopNav/TopNav.js
@@ -3,18 +3,20 @@ import { FaPhoneAlt, FaEnvelopeOpen, FaFacebook, FaTwitter, FaLinkedin, FaInstag
 
 const TopNav = () => {
     return (
-        <div className='bg-black p-6'>
+        <div className='bg-[#002060] p-6'>
             <div className='container mx-auto flex justify-between'>
-                <div className='flex items-center gap-[20px]'>
-                    <div className='flex items-center gap-[8px]'>
-                        <FaPhoneAlt className='text-white' />
-                       <a href='tel:+91 9629830350' className='text-white'>+91 9629830350</a>
-                    </div>
-                    <div>
+                <div className='hidden lg:block'>
+                    <div className='flex items-center gap-[20px]'>
                         <div className='flex items-center gap-[8px]'>
-                            <FaEnvelopeOpen className='text-white' />
-                            <a href="mailto:info@mindworksbd.com" class="text-white">info@mindworksbd.com</a>
+                            <FaPhoneAlt className='text-white' />
+                        <a href='tel:+91 9629830350' className='text-white'>+91 9629830350</a>
+                        </div>
+                        <div>
+                            <div className='flex items-center gap-[8px]'>
+                                <FaEnvelopeOpen className='text-white' />
+                                <a href="mailto:info@mindworksbd.com" class="text-white">info@mindworksbd.com</a>
 
+                            </div>
                         </div>
                     </div>
                 </div>
