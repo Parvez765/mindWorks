16ab0010ef4936a871b9419725fc9a9bcbe80f4a
tantaana <shariartanvirofficial@gmail.com>
2023-06-15 17:10:38 +0600

    bang

--- src/Component/Shared/Navbar/Navbar.js
+++ src/Component/Shared/Navbar/Navbar.js
@@ -4,112 +4,6 @@ import NavDrawer from "./NavDrawer/NavDrawer";
 
 const NavBar = () => {
   return (
-    // <nav className="navbar">
-    //   <div className="container mx-auto navContainer">
-    //     <div className="">
-    //       <img
-    //         src="https://i.ibb.co/yWx2LpH/navLogo.png"
-    //         alt="Mind WorksBd Navbar Logo"
-    //         className="w-[300px]"
-    //       />
-    //     </div>
-    //     <div
-    //       className={`navbar-toggle ${isOpen ? "open" : ""}`}
-    //       onClick={toggleNavbar}
-    //     >
-    //       <span></span>
-    //       <span></span>
-    //       <span></span>
-    //     </div>
-    //     <div
-    //       className={`navbar-menu ${isOpen ? "open" : ""}`}
-    //       onMouseLeave={handleMouseLeave}
-    //     >
-    //       <ul className="navbar-nav">
-    //         <li
-    //           className={`nav-item dropdown ${
-    //             activeDropdown === "services" ? "active" : ""
-    //           }`}
-    //           onMouseEnter={() => handleMouseEnter("services")}
-    //         >
-    //           <button
-    //             className="dropdown-toggle serviceToggle flex items-center gap-[6px]"
-    //             onClick={toggleNavbar}
-    //             onBlur={() => setIsOpen(false)}
-    //           >
-    //             Services <FaAngleDown />
-    //           </button>
-    //           <ul
-    //             className={`dropdown-menu ${
-    //               activeDropdown === "services" ? "open" : ""
-    //             }`}
-    //           >
-    //             <li className="nav-item">
-    //               <a
-    //                 href="#service1"
-    //                 className="nav-links"
-    //                 onClick={handleLinkClick}
-    //               >
-    //                 Consulting
-    //               </a>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a
-    //                 href="#service2"
-    //                 className="nav-links"
-    //                 onClick={handleLinkClick}
-    //               >
-    //                 MSI
-    //               </a>
-    //             </li>
-    //             <li className="nav-item">
-    //               <a
-    //                 href="#service3"
-    //                 className="nav-links"
-    //                 onClick={handleLinkClick}
-    //               >
-    //                 Training
-    //               </a>
-    //             </li>
-    //           </ul>
-    //         </li>
-    //         <li
-    //           className={`nav-item ${
-    //             activeDropdown === "about" ? "active" : ""
-    //           }`}
-    //           onMouseEnter={() => handleMouseEnter("about")}
-    //         >
-    //           <a href="#about" className="nav-link" onClick={handleLinkClick}>
-    //             Seed
-    //           </a>
-    //         </li>
-    //         <li
-    //           className={`nav-item ${
-    //             activeDropdown === "contact" ? "active" : ""
-    //           }`}
-    //           onMouseEnter={() => handleMouseEnter("contact")}
-    //         >
-    //           <a href="#contact" className="nav-link" onClick={handleLinkClick}>
-    //             Lean Transformation
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#contact" className="nav-link" onClick={handleLinkClick}>
-    //             About Us
-    //           </a>
-    //         </li>
-    //         <li className="nav-item">
-    //           <a href="#contact" className="nav-link" onClick={handleLinkClick}>
-    //             Contact
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //     <div className="ml-[16px]">
-    //       <button className="appoinmentBtn">Schedule a call</button>
-    //     </div>
-    //   </div>
-    // </nav>
     <div className="sticky top-0 z-40 py-4 px-4 xl:px-10 2xl:px-20 grid grid-cols-5 xl:grid-cols-4 bg-blue-900">
       <div className="flex justify-start items-center">
         <Link to="/">
@@ -144,6 +38,12 @@ const NavBar = () => {
               <NavLink to="services/msi">
                 <h2 className="text-white hover:text-blue-300 border-r border-transparent hover:border-blue-300 text-left lg:text-[12px] 2xl:text-[15px] font-bold tracking-wide uppercase duration-300 ease-in-out mb-4">
                   MSI
+                </h2>
+              </NavLink>
+
+              <NavLink to="services/training">
+                <h2 className="text-white hover:text-blue-300 border-r border-transparent hover:border-blue-300 text-left lg:text-[12px] 2xl:text-[15px] font-bold tracking-wide uppercase duration-300 ease-in-out mb-4">
+                  Training
                 </h2>
               </NavLink>
             </div>
