100fb7d3a9fca363af6e16c589d2871b962b2df6
Pervez <pervezhossain1998@gmail.com>
2023-06-17 00:53:48 +0600

    Margin Added

--- src/Component/OurServices/OurServices.js
+++ src/Component/OurServices/OurServices.js
@@ -6,7 +6,7 @@ const OurServices = () => {
     return (
         <div className='serviceContainer'>
             <div className='container mx-auto p-10'>
-                <h1 className='text-center serviceHead'>Our Services</h1>
+                <h1 className='text-center serviceHead mb-[100px]'>Our Services</h1>
                 <div className='mt-[50px]'>
                     <div className='grid grid-cols-1 lg:grid-cols-2 justify-between gap-[20px] lg:gap-[50px] mb-[50px]'>
                         <img src="https://i.ibb.co/5Lnwsjc/counselling-One.webp" alt="Our Service List One" className='lg:w-[700px] imageOne' data-aos="fade-right"/>
@@ -24,7 +24,7 @@ const OurServices = () => {
                         </div>
                         <img src="https://i.ibb.co/nDZdgJS/counseling-Two.webp" data-aos="fade-left" alt="Our Service List One" className='lg:w-[700px] imageOne' />
                     </div>
-                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-between gap-[20px] lg:gap-[50px] mb-[50px] mt-[48px]'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-between gap-[20px] lg:gap-[50px] mt-[100px]'>
                         <img src="https://i.ibb.co/3fHFqPZ/plan-planning-strategy-bysiness-ideas-concept-min.webp" data-aos="fade-right" alt="Our Service List One" className='lg:w-[700px] imageOne' />
                         <div data-aos="fade-left">
                             <h3 className='imageHeadingOne'>Traning</h3>
@@ -32,7 +32,7 @@ const OurServices = () => {
                             <div className='mt-[12px]'><button className='flex items-center learnBtn'>Learn More <FaAngleRight/></button></div>
                         </div>
                     </div>
-                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-between  gap-[20px] lg:gap-[50px]'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 justify-between  gap-[20px] lg:gap-[50px] mt-[100px]'>
                         <div data-aos="fade-right">
                             <h3 className='imageHeadingOne leading-[45px]'>COACHING & MENTORING</h3>
                             <p className='imageHeadingPara'>One-on-one life coaching for C-level professionals that enhance personal development and professional salability.</p>
