e9f0aed9dc1938cd706671418a58eec8816dcbc1
Pervez <pervezhossain1998@gmail.com>
2023-07-15 15:33:18 +0600

    Pdf Rearranged

--- src/Component/LeanTransformation/LeanTransformation.js
+++ src/Component/LeanTransformation/LeanTransformation.js
@@ -6,7 +6,7 @@ const LeanTransformation = () => {
  
 
   return (
-    <div className="mt-[50px]">
+    <div className="mt-[50px] p-10">
       <div className="container mx-auto">
         <img src="https://i.ibb.co/16RFXBm/Mindworksbd-Lean-Transformation.jpg" alt="Mind works Pdf" className="w-[650px] block mx-auto"/>
       </div>
