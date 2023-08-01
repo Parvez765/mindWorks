a356c7476e2a3b7f371b7e34ae35cce5269b2704
Pervez <pervezhossain1998@gmail.com>
2023-08-01 20:03:49 +0600

    Blog Page Fixed

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -12,11 +12,13 @@ const Blog = () => {
       </h1>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-[40px] mt-[50px]">
         {blogs?.map((blog) => (
-             <Link to={`/blog/${blog.id}`}><img src={blog?.img} alt="Mind works Blog Page" className="blogImage"/>
+            <div className="blogContainer lg:p-[10px]">
+               <Link to={`/blog/${blog.id}`}><img src={blog?.img} alt="Mind works Blog Page" className="blogImage"/>
               <h2 className="text-[35px] mt-[10px] mb-[10px] font-bold">{blog?.title}</h2>
               <p>{blog?.details.slice(0, 245)}</p>
              
              </Link>
+            </div>
          ))}
       </div>
     </div>
